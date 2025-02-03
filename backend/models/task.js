a560f3e685969466f53a0bae4142ec54e8b80160
sbrocck <sarah.brock66@gmail.com>
2025-02-02 16:01:46 -0800

    task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,14 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Define the schema for a task in the database
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  dueDate: { type: Date },
-  assignedTo: { type: String },
-  priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
-  status: { type: String, enum: ['To Do', 'In Progress', 'Completed'], default: 'To Do' }
-});
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: String,
+    dueDate: Date,
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    status: {
+      type: String,
+      enum: ["to-do", "in-progress", "completed"],
+      default: "to-do"
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium"
+    }
+  },
+  { timestamps: true }
+);
 
-// Export the model to be used in other files
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
