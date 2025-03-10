559f5f6c337ad5ca1734b78a74867ab2bd248649
sbrocck <sarah.brock66@gmail.com>
2025-03-09 22:01:25 -0700

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];  // Bearer <token>
   if (!token) return res.status(401).json({ message: "Access denied." });
 
+  
   try {
     const decoded = jwt.verify(token, "secretkey");
     req.userId = decoded.userId;
