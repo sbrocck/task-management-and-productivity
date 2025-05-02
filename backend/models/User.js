481a7f6c617fcf65d169bbcfe86811acbd6e79ec
sbrocck <sarah.brock66@gmail.com>
2025-05-01 21:02:13 -0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,11 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: { type: String, required: true, unique: true },
+  name: { type: String },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+  password: { type: String, required: true },
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }]
 }, { timestamps: true });
 
 userSchema.pre("save", async function (next) {
