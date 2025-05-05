050bde1911f9e01723a11fd03b77bd337747e77d
sbrocck <sarah.brock66@gmail.com>
2025-05-04 19:40:50 -0700

    Update task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema(
+const TaskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  completed: { type: Boolean, default: false },
+  dueDate: Date,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
+
   {
     title: {
       type: String,
