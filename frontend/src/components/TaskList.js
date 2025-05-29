ddd3a4d762bd4bbfc2cae7e6d13c8c26529cd12f
sbrocck <sarah.brock66@gmail.com>
2025-05-29 16:48:12 -0700

    Update TaskList.js

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const TaskList = ({ tasks }) => {
-    return (
-        <ul>
-            {tasks.length === 0 ? (
-                <li>No tasks yet.</li>
-            ) : (
-                tasks.map((task, index) => (
-                    <li key={index}>{task}</li>
-                ))
-            )}
-        </ul>
-    );
+  return (
+    <ul>
+      {tasks.length === 0 ? (
+        <li>No tasks yet.</li>
+      ) : (
+        tasks.map((task) => (
+          <li key={task._id || task.id || task.title}>{task.title || task}</li>
+        ))
+      )}
+    </ul>
+  );
 };
 
 export default TaskList;
