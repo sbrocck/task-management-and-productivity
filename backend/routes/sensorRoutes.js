7548390c6653bb777478e4f5dd72378e2c511dac
sbrocck <sarah.brock66@gmail.com>
2025-05-29 16:44:53 -0700

    Update sensorRoutes.js

--- backend/routes/sensorRoutes.js
+++ backend/routes/sensorRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const sensorController = require('../controllers/sensorController');
 
 // POST route to receive sensor data
-router.post('/sensor-data', sensorController.receiveSensorData);
+router.post('/sensor-data', sensorController.receiveData);
 
-// (Optional) GET route to retrieve all sensor data
+// GET route to retrieve all sensor data
 router.get('/sensor-data', sensorController.getSensorData);
 
 module.exports = router;
